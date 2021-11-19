e4a555c5f4cffad20f820ce75a6aa47d1f72dfeb
AugustoMau <augustomauro.cba@gmail.com>
2021-11-19 17:59:01 -0300

    Update Front1

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
-import "../../../src/assets/css/styles.scss";
+import "../../assets/css/styles.scss";
 //import dataSliderProduct from "./dataSliderProduct";
-import Controls from "./controls";
+import imgRightArrow from "../../assets/images/Icon feather-arrow-right-circle.png";
+import imgLeftArrow from "../../assets/images/Icon feather-arrow-left-circle.png";
 import Background from "../../assets/images/section3-image1.png";
 
-export default function Product() {
-  const [slideIndex, setSlideIndex] = useState(1)
+export default () => {
+  const [slideArrow, setSlideArrow] = useState(0);
 
-    const nextSlide = () => {
-        if(slideIndex === - 1){
-            setSlideIndex(1)
-        } 
-        else {
-          setSlideIndex(slideIndex - 1);
-        }
-        }
-    
+  const moveLeft = () => {
+    if (slideArrow === -1) {
+      setSlideArrow(1);
+    } else {
+      setSlideArrow(slideArrow - 1);
+    }
+  };
 
-    const prevSlide = () => {
-        if(slideIndex === 1){
-            setSlideIndex(-1)
-        }
-        else {
-          setSlideIndex (slideIndex +  1)}
-        }
-    
+  const moveRight = () => {
+    if (slideArrow === 1) {
+      setSlideArrow(-1);
+    } else {
+      setSlideArrow(slideArrow + 1);
+    }
+  };
 
-  return /*slideIndex === 0 */(
-    <div id="product">
-      <img className="product-img" src={Background} />
-      <div className="product-text">
+  console.log(slideArrow);
+
+  return slideArrow === 0 ? (
+    <div id="product" className="product">
+    <img className="product-img" src={Background} />
+       <div className="product-text">
         <h3 className="product-h3">
             Sed ut perspiciatis
         </h3>
@@ -40,13 +40,88 @@ export default function Product() {
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
           accusantium doloremque laudantium, totam rem aperiam.
         </p>
-        <div className="product-controls">
-        <Controls moveSlide={prevSlide} direction={"prev"}/>
-        <Controls moveSlide={nextSlide} direction={"next"}/>
-        </div>
-      </div>
-      
+              <div className="product-controls">
+                <a>
+                  <img
+                    src={imgLeftArrow}
+                    alt="flecha izquierda"
+                    onClick={moveLeft}
+                  />
+                </a>
+                <a>
+                  <img
+                    src={imgRightArrow}
+                    alt="flecha derecha"
+                    onClick={moveRight}
+                  />
+                </a>
+              </div>
+            </div>
     </div>
-    
-  )
-}
+  ) : slideArrow < 0 ? (
+    <div id="product" className="product">
+    <img className="product-img" src={Background} />
+       <div className="product-text">
+        <h3 className="product-h3">
+            
+        </h3>
+        <h2 className="product-h2">
+            TRABAJO PIN INTEGRADOR
+        </h2>
+        <p className="product-p">
+          
+        </p>
+              <div className="product-controls">
+                <a>
+                  <img
+                    src={imgLeftArrow}
+                    alt="flecha izquierda"
+                    onClick={moveLeft}
+                  />
+                </a>
+                <a>
+                  <img
+                    src={imgRightArrow}
+                    alt="flecha derecha"
+                    onClick={moveRight}
+                  />
+                </a>
+              </div>
+            </div>
+    </div>
+  ) : (
+    <div id="product" className="product">
+    <img className="product-img" src={Background} />
+       <div className="product-text">
+        <h3 className="product-h3">
+            INTEGRANTES
+        </h3>
+        <h2 className="product-h2_2">
+            AGUSTIN VALINOTI - 21 AÑOS
+            AUGUSTO MAURO - 27 AÑOS
+            EMANUEL AGUIRRE - 
+        </h2>
+        <p className="product-p">
+            MUNDOS E
+        </p>
+              <div className="product-controls">
+                <a>
+                  <img
+                    src={imgLeftArrow}
+                    alt="flecha izquierda"
+                    onClick={moveLeft}
+                  />
+                </a>
+                <a>
+                  <img
+                    src={imgRightArrow}
+                    alt="flecha derecha"
+                    onClick={moveRight}
+                  />
+                </a>
+              </div>
+            </div>
+    </div>
+
+  );
+};
